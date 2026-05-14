498c3156ec9b59e36e5c9628ff6ba7fc2a9fbbbc
geofranzi <geofranzi@users.noreply.github.com>
2026-05-14 08:19:20 +0000

    deploy: bf3ed7a132d21a58c53242a6e2dcfe432296c7bf

--- _app/immutable/nodes/6.325669f0.js
+++ _app/immutable/nodes/6.325669f0.js
@@ -1,4 +1,4 @@
-import{S as Q,i as F,s as J,k as a,q as l,a as G,l as i,m as b,r as c,h as m,c as K,n as d,a1 as X,b as Y,D as e,W as j}from"../chunks/index.58f29621.js";function Z(M){let o,r,s,u,w,y,h,A,B,t,N,D,_,x,R,k,q,W,I,T,z,E,O,S,V,H,L,P,U;return{c(){o=a("div"),r=a("div"),s=a("div"),u=a("h1"),w=l("Vision statement"),y=G(),h=a("img"),B=G(),t=a("blockquote"),N=l(`Our vision is to create a future where the Namib Desert in Namibia stands as a beacon of
+import{S as Q,i as F,s as J,k as a,q as l,a as G,l as o,m as b,r as c,h as m,c as K,n as d,a1 as X,b as Y,D as e,W as j}from"../chunks/index.58f29621.js";function Z(M){let i,r,s,u,w,y,h,A,B,t,N,D,_,x,R,k,q,W,I,T,z,E,O,S,V,H,L,P,U;return{c(){i=a("div"),r=a("div"),s=a("div"),u=a("h1"),w=l("Vision statement"),y=G(),h=a("img"),B=G(),t=a("blockquote"),N=l(`Our vision is to create a future where the Namib Desert in Namibia stands as a beacon of
         environmental resilience and serves as a model for sustainable conservation worldwide.
         `),D=a("br"),_=a("br"),x=l(`
         We envision a Namib Desert where the Namaqua chameleons thrive, their populations
@@ -36,42 +36,42 @@ import{S as Q,i as F,s as J,k as a,q as l,a as G,l as i,m as b,r as c,h as m,c a
         `),L=a("br"),P=a("br"),U=l(`
         Together, we can realize this vision and ensure the Namib Desert's enduring legacy as a
         testament to the triumph of environmental conservation and the boundless possibilities of a
-        sustainable future.`),this.h()},l(f){o=i(f,"DIV",{});var g=b(o);r=i(g,"DIV",{class:!0});var v=b(r);s=i(v,"DIV",{class:!0});var p=b(s);u=i(p,"H1",{class:!0});var C=b(u);w=c(C,"Vision statement"),C.forEach(m),y=K(p),h=i(p,"IMG",{src:!0,alt:!0,width:!0}),p.forEach(m),B=K(v),t=i(v,"BLOCKQUOTE",{class:!0});var n=b(t);N=c(n,`Our vision is to create a future where the Namib Desert in Namibia stands as a beacon of
+        sustainable future.`),this.h()},l(f){i=o(f,"DIV",{});var g=b(i);r=o(g,"DIV",{class:!0});var v=b(r);s=o(v,"DIV",{class:!0});var p=b(s);u=o(p,"H1",{class:!0});var C=b(u);w=c(C,"Vision statement"),C.forEach(m),y=K(p),h=o(p,"IMG",{src:!0,alt:!0,width:!0}),p.forEach(m),B=K(v),t=o(v,"BLOCKQUOTE",{class:!0});var n=b(t);N=c(n,`Our vision is to create a future where the Namib Desert in Namibia stands as a beacon of
         environmental resilience and serves as a model for sustainable conservation worldwide.
-        `),D=i(n,"BR",{}),_=i(n,"BR",{}),x=c(n,`
+        `),D=o(n,"BR",{}),_=o(n,"BR",{}),x=c(n,`
         We envision a Namib Desert where the Namaqua chameleons thrive, their populations
         restored and their critical role in controlling the tenebroid beetle species ensured. A future
         where road users in areas such as the roads to Sesriem out of Walvis Bay and the Boshua
         Pass exit out of Swakop towards Langer Heinrich mine prioritize the safety and protection of
         these chameleons, understanding their significance to the delicate balance of the desert
         ecosystem.
-        `),R=i(n,"BR",{}),k=i(n,"BR",{}),q=c(n,`
+        `),R=o(n,"BR",{}),k=o(n,"BR",{}),q=c(n,`
         In our vision, the Namib Desert becomes a living laboratory, where our research and
         monitoring efforts yield insights that guide effective conservation strategies. By
         understanding the impacts of climate change and other environmental factors, we contribute
         to the development of innovative solutions that safeguard the desert's micro fauna and flora,
         ensuring their resilience in the face of a changing world.
-        `),W=i(n,"BR",{}),I=i(n,"BR",{}),T=c(n,`
+        `),W=o(n,"BR",{}),I=o(n,"BR",{}),T=c(n,`
         We aspire to inspire the youth to become environmental leaders and advocates, instilling in
         them a deep appreciation for the Namib Desert's beauty and fragility. Through our educative
         tourism and outreach programs, we empower young people with knowledge, fostering a sense
         of responsibility and a commitment to sustainable practices. These youth become the driving
         force behind the future preservation and stewardship of the Namib Desert, ensuring its
         protection for generations to come.
-        `),z=i(n,"BR",{}),E=i(n,"BR",{}),O=c(n,`
+        `),z=o(n,"BR",{}),E=o(n,"BR",{}),O=c(n,`
         In our vision, Nameleon emerges as a trusted global leader in environmental conservation. By
         forging strong partnerships with local communities, government agencies, and stakeholders,
         we influence policies and practices that prioritize the sustainable management of natural
         resources and address the challenges posed by climate change. Our collective efforts lead to
         the adoption of holistic approaches and the integration of environmental conservation into
         every aspect of society.
-        `),S=i(n,"BR",{}),V=i(n,"BR",{}),H=c(n,`
+        `),S=o(n,"BR",{}),V=o(n,"BR",{}),H=c(n,`
         Ultimately, our vision extends beyond the borders of Namibia. We aspire to inspire a global
         movement for environmental protection, where the lessons learned from the Namib Desert's
         conservation journey inform and guide efforts to preserve and restore fragile ecosystems
         worldwide. By demonstrating the power of collaboration, education, and sustainable
         practices, we strive to create a world where nature and humanity coexist in harmony.
-        `),L=i(n,"BR",{}),P=i(n,"BR",{}),U=c(n,`
+        `),L=o(n,"BR",{}),P=o(n,"BR",{}),U=c(n,`
         Together, we can realize this vision and ensure the Namib Desert's enduring legacy as a
         testament to the triumph of environmental conservation and the boundless possibilities of a
-        sustainable future.`),n.forEach(m),v.forEach(m),g.forEach(m),this.h()},h(){d(u,"class","content-heading"),X(h.src,A="static/images/vision-svgrepo-com.svg")||d(h,"src",A),d(h,"alt","..."),d(h,"width","40px"),d(s,"class","flex"),d(t,"class","content-blockquote"),d(r,"class","content-div")},m(f,g){Y(f,o,g),e(o,r),e(r,s),e(s,u),e(u,w),e(s,y),e(s,h),e(r,B),e(r,t),e(t,N),e(t,D),e(t,_),e(t,x),e(t,R),e(t,k),e(t,q),e(t,W),e(t,I),e(t,T),e(t,z),e(t,E),e(t,O),e(t,S),e(t,V),e(t,H),e(t,L),e(t,P),e(t,U)},p:j,i:j,o:j,d(f){f&&m(o)}}}class ee extends Q{constructor(o){super(),F(this,o,null,Z,J,{})}}export{ee as component};
+        sustainable future.`),n.forEach(m),v.forEach(m),g.forEach(m),this.h()},h(){d(u,"class","content-heading"),X(h.src,A="/images/vision-svgrepo-com.svg")||d(h,"src",A),d(h,"alt","..."),d(h,"width","40px"),d(s,"class","flex"),d(t,"class","content-blockquote"),d(r,"class","content-div")},m(f,g){Y(f,i,g),e(i,r),e(r,s),e(s,u),e(u,w),e(s,y),e(s,h),e(r,B),e(r,t),e(t,N),e(t,D),e(t,_),e(t,x),e(t,R),e(t,k),e(t,q),e(t,W),e(t,I),e(t,T),e(t,z),e(t,E),e(t,O),e(t,S),e(t,V),e(t,H),e(t,L),e(t,P),e(t,U)},p:j,i:j,o:j,d(f){f&&m(i)}}}class ee extends Q{constructor(i){super(),F(this,i,null,Z,J,{})}}export{ee as component};
